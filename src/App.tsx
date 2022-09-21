64bcd007ddd5d03c0472e91973d263f6039670e2
Eugene Chan <me@eugenechantk.me>
2022-09-21 20:59:46 -0300

    Added magic connect instance to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { Magic } from 'magic-sdk';
+import { ConnectExtension } from '@magic-ext/connect';
 
-export default function Home() {
-  return <ConnectWallet accentColor="#f213a4" colorMode="light" />;
+const App = () => {
+  
+  const magic = new Magic('pk_live_257001A814ED44DA', {
+    extensions: [new ConnectExtension()],
+    network: "rinkeby",
+  });
+
+  return (
+    <>
+      <ConnectWallet accentColor="#f213a4" colorMode="light" />
+    </>
+  )
 }
+
+export default App;
