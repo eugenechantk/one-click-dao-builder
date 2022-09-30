b9b1a8d25ebc5357b68b869b2e580266916fb026
Eugene Chan <me@eugenechantk.me>
2022-09-30 00:11:15 -0300

    Added controller, engines, and helper functions for connecting to Ethereum blockchain

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import WalletConnect from "@walletconnect/client";
+import { getAppControllers } from "./controllers";
+import { DEFAULT_ACTIVE_INDEX } from "./constraints/default";
 
 export interface IAppState {
   loading: boolean;
@@ -22,6 +24,9 @@ export interface IAppState {
   results: any[];
   payload: any;
 }
+
+export const DEFAULT_ACCOUNTS = getAppControllers().wallet.getAccounts();
+export const DEFAULT_ADDRESS = DEFAULT_ACCOUNTS[DEFAULT_ACTIVE_INDEX];
 
 const App = () => {
 
---
+++ src/controllers/index.ts
@@ -0,0 +1,21 @@
+import { WalletController, getWalletController } from "./wallet";
+
+interface IAppControllers {
+  wallet: WalletController;
+}
+
+let controllers: IAppControllers | undefined;
+
+export function setupAppControllers(): IAppControllers {
+  const wallet = getWalletController();
+  controllers = { wallet };
+  return controllers;
+}
+
+export function getAppControllers(): IAppControllers {
+  let _controllers = controllers;
+  if (!_controllers) {
+    _controllers = setupAppControllers();
+  }
+  return _controllers;
+}
