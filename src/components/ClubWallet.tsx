395a3b8077e8ac633368a3ce58e36cda1baf7266
Eugene Chan <me@eugenechantk.me>
2022-10-08 18:33:17 -0300

    Removed unnecessary imports from ClubWallet component

--- src/components/ClubWallet.tsx
+++ src/components/ClubWallet.tsx
@@ -1,9 +1,5 @@
 import { useEffect } from "react";
-import Moralis from "moralis";
 import { getAppControllers } from "../controllers";
-import axios from "axios";
-import { getAppConfig } from "../config";
-import { getChainData } from "../helpers/utilities";
 
 export const ClubWallet = () => {
   useEffect(() => {
@@ -11,8 +7,7 @@ export const ClubWallet = () => {
         const balance = await getAppControllers().wallet.getAllBalance();
         console.log(balance);
     }
-    getBalance()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    getBalance();
   }, []);
 
   return <>Club Wallet info</>;
