8f6fa5742ee2adac3d1a459894679f8ec70b38cd
Eugene Chan <me@eugenechantk.me>
2022-10-09 15:46:13 -0300

    implemented fetchBalance function in ClubWallet component

--- src/components/ClubWallet.tsx
+++ src/components/ClubWallet.tsx
@@ -5,22 +5,15 @@ import { IBalanceData } from "../controllers/wallet";
 export const ClubWallet = () => {
   const provider = getAppControllers().provider;
   const [balance, setBalance] = useState<IBalanceData[]>([]);
-  const filter = {
-    address: getAppControllers().wallet.getAddress(),
-    topic: null,
+
+  const fetchBalance = async () => {
+    const newBalance = await getAppControllers().wallet.getAllBalance();
+    setBalance(newBalance);
+    // setTimeout(fetchBalance, 10000);
   }
-  provider.on(filter, async () => {
-    console.log('New event emitted with club wallet');
-    const balance = await getAppControllers().wallet.getAllBalance();
-    setBalance(balance);
-  })
+  
 
   useEffect (() => {
-    console.log(getAppControllers().wallet.getAddress());
-    const fetchBalance = async () => {
-      const balance = await getAppControllers().wallet.getAllBalance();
-      setBalance(balance);
-    }
     fetchBalance()
   },[]);
 
