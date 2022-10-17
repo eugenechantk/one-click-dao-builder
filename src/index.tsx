87b8d4e3ddd7dec3ff8950890191fede75fd35b9
Eugene Chan <me@eugenechantk.me>
2022-10-17 17:54:30 -0300

    Refactored the structure of the whole app to have WrappedApp wrapping both login and other parts of the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  ChainId,
-  ThirdwebSDKProvider,
-} from "@thirdweb-dev/react";
 import { Magic } from "magic-sdk";
 import { ConnectExtension } from "@magic-ext/connect";
 import { getChainData } from "./helpers/utilities";
 import { ethers } from "ethers";
 import { getAppConfig } from "./config";
+import { WrappedApp } from "./WrappedApp";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
-
-const INFURA_KEY = String(process.env.REACT_APP_INFURA_PROJECT_ID);
 
 const MAGIC_API_KEY = String(process.env.REACT_APP_MAGIC_API_KEY);
 const customNodeOptions = {
@@ -23,31 +15,21 @@ const customNodeOptions = {
   chainId: getChainData(getAppConfig().chainId).network_id,
 };
 
-export const magic = new Magic(MAGIC_API_KEY, {
+const magic = new Magic(MAGIC_API_KEY, {
   network: customNodeOptions,
   extensions: [new ConnectExtension()],
 });
 
 // @ts-ignore
-export const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
+const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
 
-export const signer = provider.getSigner();
+
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <ThirdwebSDKProvider
-      desiredChainId={activeChainId}
-      provider={provider}
-      signer={signer}
-      sdkOptions={{readonlySettings:{rpcUrl:"https://goerli.infura.io/v3/%API_KEY%".replace(
-        "%API_KEY%",
-        INFURA_KEY
-      ), chainId:5}}}
-    >
-      <App />
-    </ThirdwebSDKProvider>
+    <WrappedApp provider={provider} magic={magic}/>
   </React.StrictMode>
 );
 
